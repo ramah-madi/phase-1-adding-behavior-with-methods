0602632df31387d74cda38688cd31f0ede545c07
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-03-09 21:18:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+// Dog class
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+// Bird class
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (this.sex === 'male') {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
